4ca2ffdeaf09583655cd69950c3d7c6a4107928f
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2020-06-21 14:38:00 +0200

    client: JSON parsing, block checking

--- html-client/app.js
+++ html-client/app.js
@@ -22,6 +22,51 @@ function readForm() {
     };
 }
 
+function checkBlock(last_block_bytes_list, nonce_bytes_list, payload_bytes_list, required_difficulty) {
+    var hash_msg = last_block_bytes_list + nonce_bytes_list + payload_bytes_list;
+    var block_hex = sha256(hash_msg);
+    console.log('block_hex', block_hex);
+    var achieved_difficulty = 0;
+    var was_zero = true;
+    for (var i = 0; was_zero && i < block_hex.length; i++) {
+        var c = block_hex[i];
+        was_zero = false;
+        switch (c) {
+        case '0':
+            achieved_difficulty += 4;
+            was_zero = true;
+            break;
+        case '1':
+            achieved_difficulty += 3;
+            break;
+        case '2':
+        case '3':
+            achieved_difficulty += 2;
+            break;
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+            achieved_difficulty += 1;
+            break;
+        case '8':
+        case '9':
+        case 'a':
+        case 'b':
+        case 'c':
+        case 'd':
+        case 'e':
+        case 'f':
+            break;
+        default:
+            throw `Unknown hexit ${c} at position ${i} of hexhash ${block_hex}?!`;
+        }
+    }
+    console.log('achieved_difficulty', achieved_difficulty, ', required_difficulty', required_difficulty);
+
+    return achieved_difficulty >= required_difficulty;
+}
+
 function trySubmit() {
     console.log("Parsing ...");
     var formdata = readForm();
@@ -32,9 +77,12 @@ function trySubmit() {
         new Request(pixel_url)
         //new Request(pixel_url, {method: 'POST', body: '{"A": "B", "C": 42, asdfasdf asdf asdfq2t2h h4}'})
     ).then(response => {
-        console.log('Request result:');
         console.log(response);
-        // JSON.parse(response.body);
+        return response.json();
+    }).then(response_json => {
+        console.log('Request result:');
+        console.log(response_json);
+        //lastBlock: "7e49c83b400ab55e405f7396162309c0e311ee0eba248f960878c1d729987ff1", requiredDifficulty: 8, rgb: 0
         console.error("NOT IMPLEMENTED");
     }).catch(error => {
         console.error(error);
